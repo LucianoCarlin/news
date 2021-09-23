38597175a43ade5b4d2cc7e5753a32cfe2ba3c86
LucianoCarlin <lucianoalvescarlin@gmail.com>
2021-09-23 10:32:13 -0300

    integraçao api strip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,39 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable @next/next/no-document-import-in-page */
+import { GetServerSideProps } from 'next'
 import type { NextPage } from 'next'
+import { SubscribeButton } from '../components/SubscribeButton'
+
+import styles from './home.module.scss'
+import { stripe } from '../services/stripe'
 
 const Home: NextPage = () => {
   return (
-    <h1>oi</h1>
+    <>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span>World</h1>
+          <p>
+            Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros.<br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
   )
 }
-
 export default Home
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JcqYFFpaRpHBQOUkAMNRD3v', {
+    expand: ['product']
+  })
+  return {
+    props: {
+      name: 'Luciano',
+    }
+  }
+}
