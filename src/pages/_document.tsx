65fdaec63543ab9771c6100598494a8ea9e444ed
luciano <luciano@debian-BULLSEYE-live-builder-AMD64>
2021-09-22 22:12:20 -0300

    add component Header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/google-font-preconnect */
 /* eslint-disable @next/next/google-font-display */
 import React from 'react'
 import Document, { Html, Main, Head, NextScript } from 'next/document'
@@ -5,10 +6,11 @@ import Document, { Html, Main, Head, NextScript } from 'next/document'
 export default class MyDocument extends Document {
   render() {
     return(
-      <Html lang="pt">
+      <Html>
         <Head>
+          <link rel="preconnet" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet" />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="shortcut icon" href="/favicon.png" type="image/png"/>
         </Head>
         <body>
           <Main />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
 import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      
-    </div>
+    <h1>oi</h1>
   )
 }
 
